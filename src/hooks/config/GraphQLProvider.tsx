d557adf3b458f4a26b97a128354cbcc1a4189ce4
tnyo43 <tnyo43@gmail.com>
2022-05-04 15:58:04 +0900

    apolloを削除

--- src/hooks/config/GraphQLProvider.tsx
+++
@@ -1,7 +0,0 @@
-import { ApolloProvider } from '@apollo/client';
-import React, { ReactNode } from 'react';
-import { graphqlClient } from '~/apis/gql';
-
-export const GraphQLProvider = (props: { children: ReactNode }) => (
-  <ApolloProvider client={graphqlClient}>{props.children}</ApolloProvider>
-);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { GraphQLProvider } from '~/hooks/config/GraphQLProvider';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <GraphQLProvider>
-    <Component {...pageProps} />
-  </GraphQLProvider>
+  <Component {...pageProps} />
 );
 
 export default MyApp;
